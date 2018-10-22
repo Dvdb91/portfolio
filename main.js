f6ad88be5d1c413bd6bd3a571726dfefaf0ce0f5
dries <driesv91@gmail.com>
2018-10-22 22:44:52 +0200

    adding javascript file

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+
+// console.log("hallohallo")
+
+// var currentScrollpos = window.pageYOffset;
+// const container = document.getElementById("container");
+
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (currentScrollPos > 150) {
+//     container.className = 'animated fadeOutUp fast';
+//   } else {
+//     container.className = 'animated fadeIn faster';
+//   }
+
+// }
+
+// console.log("test finished")
+
+
+
+
+console.log("hallohallo")
+
+var currentScrollpos = window.pageYOffset;
+const container = document.getElementById("container");
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (currentScrollPos > 150) {
+    container.className = 'animated fadeOutUp fast';
+  } else {
+    container.className = 'animated fadeIn faster';
+  }
+
+}
+
+console.log("test finished")
+
