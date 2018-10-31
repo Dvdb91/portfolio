816cff8bae450d6bac1fde9c7126bd45a30462e2
dries <driesv91@gmail.com>
2018-10-31 06:31:37 +0200

    updating js file

--- main.js
+++ main.js
@@ -22,20 +22,40 @@
 
 
 
-console.log("hallohallo")
+// console.log("hallohallo")
 
 var currentScrollpos = window.pageYOffset;
 const container = document.getElementById("container");
+const oliviaNav = document.getElementById("navigation"); 
+var previousScrollPos = 0;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+
+//   if (currentScrollPos > 150) {
+//     container.className = 'animated fadeOutUp fast';
+//   } else {
+//     container.className = 'animated fadeIn faster';
+//   }
+ 
+// }
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (currentScrollPos > 150) {
-    container.className = 'animated fadeOutUp fast';
+
+  if (currentScrollPos >= previousScrollPos) {
+    container.className = 'animated fadeOutUp fast'
   } else {
     container.className = 'animated fadeIn faster';
   }
-
+  currentScrollpos = previousScrollPos;
 }
+
+
+
+
+
+
+
 
 console.log("test finished")
 
