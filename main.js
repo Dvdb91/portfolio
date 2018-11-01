299d01f74cf0b61555ed8f73067491200908b851
dries <driesv91@gmail.com>
2018-11-01 07:07:41 +0200

    adding scrolling effects header and title

--- main.js
+++ main.js
@@ -1,61 +1,27 @@
 
+$(document).ready
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
 
 
+mywindow.scroll(function () {
 
-// console.log("hallohallo")
+    newscroll = mywindow.scrollTop();
+    if (newscroll < 100) {
+      $('.wrapper').stop().fadeIn("slow");
+      up = !up;
 
-// var currentScrollpos = window.pageYOffset;
-// const container = document.getElementById("container");
+    } else if (newscroll > mypos && !up) {
+        $('#oliviaS').stop().fadeOut();
+        $('.wrapper').stop().fadeOut("slow");
+        up = !up;
+        console.log(up);
 
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (currentScrollPos > 150) {
-//     container.className = 'animated fadeOutUp fast';
-//   } else {
-//     container.className = 'animated fadeIn faster';
-//   }
-
-// }
-
-// console.log("test finished")
-
-
-
-
-// console.log("hallohallo")
-
-var currentScrollpos = window.pageYOffset;
-const container = document.getElementById("container");
-const oliviaNav = document.getElementById("navigation"); 
-var previousScrollPos = 0;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-
-//   if (currentScrollPos > 150) {
-//     container.className = 'animated fadeOutUp fast';
-//   } else {
-//     container.className = 'animated fadeIn faster';
-//   }
- 
-// }
-
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-
-  if (currentScrollPos >= previousScrollPos) {
-    container.className = 'animated fadeOutUp fast'
-  } else {
-    container.className = 'animated fadeIn faster';
-  }
-  currentScrollpos = previousScrollPos;
-}
-
-
-
-
-
-
-
-
-console.log("test finished")
-
+    } else if(newscroll < mypos && up) {
+        $('#oliviaS').stop().fadeIn();
+        up = !up;
+    }
+    mypos = newscroll;
+});
